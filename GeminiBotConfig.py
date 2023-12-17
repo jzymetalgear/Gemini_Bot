9def642a0e4c11bddc9b4201891b3693b3bed4dc
jzymetalgear <135740686+jzymetalgear@users.noreply.github.com>
2023-12-16 17:49:51 -0800

    Add files via upload

---
+++ GeminiBotConfig.py
@@ -0,0 +1,6 @@
+﻿#Create at https://makersuite.google.com/
+GOOGLE_AI_KEY = "AIzaSyDMKDe8gDar5GJe4ljdfTqIxqGKm89Jbro"
+#Create at https://discord.com/developers/applications
+DISCORD_BOT_TOKEN = "MTE4NDY0Mjc0MjIxNTc3MDIxMw.GqQHdI.IlOXH6Np50_FTt2eJQROjHU2Uc8mn4SxuHEcrU"
+#Disable history by setting to 0
+MAX_HISTORY = 15
